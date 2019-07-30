4c553a1c59df2eeb29b02e118141a7ec006ba92e
Conrad Beach <conradbeach@protonmail.com>
2019-07-30 10:46:37 -0600

    feature: Configure AWS credentials
    The AWS credentials and region are stored in the encrypted credentials file.
    
    I added an initializer to set the AWS config so you can create and use an AWS client object anywhere in the application and credentials will be handled already.

---
+++ config/initializers/aws_sdk.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+Aws.config.update(
+  region: Rails.application.credentials.aws[:region],
+  credentials: Aws::Credentials.new(
+    Rails.application.credentials.aws[:access_key_id],
+    Rails.application.credentials.aws[:secret_access_key],
+  ),
+)
