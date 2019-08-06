82148d17b3ca3ad663d820e76edc77e2578c2d08
Conrad Beach <conradbeach@protonmail.com>
2019-08-06 13:35:24 -0600

    feature: Add logic for delivering an Alert
    This adds a `Alert#conditionally_deliver` method which delivers an alert to the owning user when conditions are met.

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -7,5 +7,41 @@
 class Alert < ApplicationRecord
   belongs_to :user
   has_many :deliveries, dependent: :destroy
+
   validates :temperature_threshold, presence: true
+
+  # Delivers the alert via SMS to the owning user if the following 2 conditions are met:
+  # 1. The given `current_temperature` is greater than or equal to the `temperature_threshold` for the alert.
+  # 2. The alert hasn't already been delivered today.
+  #
+  # @param current_temperature The current temperature for the owning user's zip code.
+  #
+  # @return The newly created `Delivery` record if the alert is delivered; `nil` otherwise.
+  def conditionally_deliver(current_temperature)
+    return if current_temperature < temperature_threshold
+    return if delivered_today?
+
+    deliver
+  end
+
+  private
+
+  def delivered_today?
+    latest_sent_at_time = deliveries.order(sent_at: :desc).pick(:sent_at)
+
+    latest_sent_at_time&.today?
+  end
+
+  def deliver
+    Aws::SNS::Client.new.publish(
+      phone_number: user.phone_number,
+      message: formatted_message,
+    )
+
+    deliveries.create(sent_at: Time.current)
+  end
+
+  def formatted_message
+    "Temperature Alert: It hit #{temperature_threshold} degrees. #{message}"
+  end
 end
