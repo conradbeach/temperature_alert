60c90b23e35e582baade8a82d8932b02b75fd1cd
Conrad Beach <conradbeach@protonmail.com>
2020-02-04 10:21:44 -0700

    feature: Configure Clearance initializer

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,4 +1,8 @@
 Clearance.configure do |config|
-  config.mailer_sender = "reply@example.com"
+  config.cookie_domain = "temperature-alert.herokuapp.com"
+  config.mailer_sender = "help@temperature-alert.herokuapp.com"
+  config.redirect_url = "/dashboard"
   config.rotate_csrf_on_sign_in = true
+  config.secure_cookie = true
+  config.user_model = "User"
 end
